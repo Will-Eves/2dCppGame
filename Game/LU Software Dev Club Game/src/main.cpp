045ffed42fbfdb60ab7b0906d5913652941f2448
Will-Eves <63985486+Will-Eves@users.noreply.github.com>
2024-10-01 19:27:55 -0400

    Fixed Import Bug

---
+++ Game/LU Software Dev Club Game/src/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode(800, 450), "SFML works!");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        // draw stuff here
+
+        window.display();
+    }
+
+    return 0;
+}
