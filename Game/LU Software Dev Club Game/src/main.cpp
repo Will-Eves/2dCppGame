fdd747895ac2e94636fbbaa65da3744aadb34f4d
Will-Eves <63985486+Will-Eves@users.noreply.github.com>
2024-10-01 19:35:50 -0400

    Made Window Bigger

--- Game/LU Software Dev Club Game/src/main.cpp
+++ Game/LU Software Dev Club Game/src/main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-    sf::RenderWindow window(sf::VideoMode(800, 450), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
 
     while (window.isOpen()) {
         sf::Event event;
