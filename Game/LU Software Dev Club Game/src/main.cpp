b5e6e8f0b3977edbbf4a9b0e7c74789f6786c6e1
Will-Eves <63985486+Will-Eves@users.noreply.github.com>
2024-10-22 18:58:49 -0400

    Update main.cpp

--- Game/LU Software Dev Club Game/src/main.cpp
+++ Game/LU Software Dev Club Game/src/main.cpp
@@ -3,6 +3,12 @@
 int main(){
     sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
 
+    sf::RectangleShape rect1 = sf::RectangleShape(sf::Vector2f(100, 100));
+
+    sf::CircleShape circ1 = sf::CircleShape(50);
+
+    circ1.setPosition(sf::Vector2f(200, 200));
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -14,6 +20,10 @@ int main(){
         window.clear();
 
         // draw stuff here
+
+        window.draw(rect1);
+
+        window.draw(circ1);
 
         window.display();
     }
