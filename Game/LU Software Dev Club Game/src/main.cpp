029a8d73591bd61712b2bc2aa6860ea4b254f150
GrahamRT <143757756+GrahamRT@users.noreply.github.com>
2024-10-22 19:16:56 -0400

    Update main.cpp

--- Game/LU Software Dev Club Game/src/main.cpp
+++ Game/LU Software Dev Club Game/src/main.cpp
@@ -3,14 +3,10 @@
 int main(){
     sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
 
-    sf::RectangleShape rect1 = sf::RectangleShape(sf::Vector2f(100, 100));
+	sf::RectangleShape rect1 = sf::RectangleShape(sf::Vector2f(100, 100));
 
-    sf::CircleShape circ1 = sf::CircleShape(50);
-
+	sf::CircleShape circ1 = sf::CircleShape(50);
     circ1.setPosition(sf::Vector2f(200, 200));
-
-    int x = 0;
-    int y = 0;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -19,31 +15,18 @@ int main(){
                 window.close();
             }
         }
-
-        // get input here
-
+        int x = 0;
+        int y = 0;
+        // get input
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
             x -= 10;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            x += 10;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            y -= 10;
-        }
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            y += 10;
         }
 
         window.clear();
 
         // draw stuff here
-
-        rect1.setPosition(sf::Vector2f(x, y));
-
-        window.draw(rect1);
-
-        window.draw(circ1);
+		window.draw(rect1);
+		window.draw(circ1);
 
         window.display();
     }
